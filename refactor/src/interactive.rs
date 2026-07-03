d8fd9d6019b8b844d0e6a6865a99272c65e603b8
Facundo Efimenco <hackkr783@gmail.com>
2026-07-02 22:08:53 -0300

    fix: restaurar método redo() completo en interactive.rs

--- refactor/src/interactive.rs
+++ refactor/src/interactive.rs
@@ -300,6 +300,15 @@ impl DesignTool {
             self.redo_stack.push_back(action);
         }
     }
+
+    /// Rehacer última acción deshecha
+    pub fn redo(&mut self, game_world: &mut GameWorld) {
+        if let Some(action) = self.redo_stack.pop_back() {
+            match &action {
+                DesignAction::PlaceBuilding { x, y, building_type, .. } => {
+                    game_world.world.spawn((
+                        Position::new(*x, *y),
+                        Renderable::rect(building_color(*building_type), 3.0, 3),
                         ConstructionState { progress: 1.0, building_type: *building_type },
                         ResourceStorage { money: 1000.0, food: 100.0, goods: 50.0 },
                     ));
@@ -325,6 +334,7 @@ impl DesignTool {
             }
             self.undo_stack.push_back(action);
         }
+    }
     }
 
     /// Agrega una acción al historial de undo
